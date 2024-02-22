8f614868b1b175633ae0ddcd6a7d21fd55148151
LeoSeiti <Leonardoseiti1@gmail.com>
2024-02-22 19:55:27 -0300

    Televison-using-flex-box

--- App.js
+++ App.js
@@ -2,23 +2,31 @@ import { View } from "react-native";
 
 function App() {
   return (
-    <View style={{ backgroundColor: "#000000", position:"absolute", width:"100%", height:"100%",padding:"2%"}}>
-      <View style={{ backgroundColor: "#ffffff",position:"absolute", width:"14.2%", height:"85%"}}> </View>
-      <View style={{ backgroundColor: "#fffb00",position:"absolute", width:"14.2%", height:"85%", left:"14%"}}> </View>
-      <View style={{ backgroundColor: "#00ffea",position:"absolute", width:"14.2%", height:"85%", left:"28%"}}> </View>
-      <View style={{ backgroundColor: "#00ff00",position:"absolute", width:"14.2%", height:"85%", left:"42%"}}> </View>
-      <View style={{ backgroundColor: "#AF33FF",position:"absolute", width:"14.2%", height:"85%", left:"56%"}}> </View>
-      <View style={{ backgroundColor: "#FF0000",position:"absolute", width:"14.2%", height:"85%", left:"70%"}}> </View>
-      <View style={{ backgroundColor: "#0100FF",position:"absolute", width:"14.2%", height:"85%", left:"84%"}}> </View>
+    
 
-      <View style={{ backgroundColor: "#0100FF",position:"absolute", width:"14.2%", height:"12%", bottom: "10.8%"}}> </View>
-      <View style={{ backgroundColor: "#000000",position:"absolute", width:"14.2%", height:"12%", bottom: "10.8%", left:"14%"}}> </View>
-      <View style={{ backgroundColor: "#AF33FF",position:"absolute", width:"14.2%", height:"12%", bottom: "10.8%", left:"28%"}}> </View>
-      <View style={{ backgroundColor: "#000000",position:"absolute", width:"14.2%", height:"12%", bottom: "10.8%", left:"42%"}}> </View>
-      <View style={{ backgroundColor: "#00ffea",position:"absolute", width:"14.2%", height:"12%", bottom: "10.8%", left:"56%"}}> </View>
-      <View style={{ backgroundColor: "#000000",position:"absolute", width:"14.2%", height:"12%", bottom: "10.8%", left:"70%"}}> </View>
-      <View style={{ backgroundColor: "#ffffff",position:"absolute", width:"14.2%", height:"12%", bottom: "10.8%", left:"84%"}}> </View>
-    </View>
+  <View style={{ backgroundColor: "#000000",display:"flex", flexDirection:"column", flex:1}}>
+        <View style={{flex:6, flexDirection:"row"}}>
+          <View style={{ backgroundColor: "#ffffff", width:"14.2%",flex:2}}> </View>
+          <View style={{ backgroundColor: "#fffb00", width:"14.2%",flex:2}}> </View>
+          <View View style={{ backgroundColor: "#00ffea", width:"14.2%",flex:2}}> </View>
+          <View style={{ backgroundColor: "#00ff00", width:"14.2%",flex:2}}> </View>
+          <View style={{ backgroundColor: "#AF33FF", width:"14.2%",flex:2}}> </View>
+          <View style={{ backgroundColor: "#FF0000", width:"14.2%",flex:2}}> </View>
+          <View style={{ backgroundColor: "#0100FF", width:"14.2%",flex:2}}> </View>
+        </View>
+
+
+
+        <View style={{ display:'flex', flexDirection:'row',flex:1}}>
+          <View style={{ backgroundColor: "#0b2d39", width:"18%"}}> </View>
+          <View View style={{ backgroundColor: "#FFFFFF", width:"18%"}}> </View>
+          <View style={{ backgroundColor: "#400474", width:"18%"}}> </View>
+          <View style={{ backgroundColor: "#000000", width:"18%"}}> </View>
+          <View style={{ backgroundColor: "#000000", width:"18%"}}> </View>
+          <View style={{ backgroundColor: "#000000", width:"18%"}}> </View>
+          <View style={{ backgroundColor: "#000000", width:"18%"}}> </View>
+        </View>
+  </View>
   );
 }
 
