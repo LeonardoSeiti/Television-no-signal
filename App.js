7911cd6a724fa4b4dfd06df009046043b65ddaff
LeoSeiti <leonardoseiti1@Gmail.com>
2024-02-22 08:49:39 -0300

    Television no signal found

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from "react-native";
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ backgroundColor: "#000000", position:"absolute", width:"100%", height:"100%",padding:"2%"}}>
+      <View style={{ backgroundColor: "#ffffff",position:"absolute", width:"14.2%", height:"85%"}}> </View>
+      <View style={{ backgroundColor: "#fffb00",position:"absolute", width:"14.2%", height:"85%", left:"14%"}}> </View>
+      <View style={{ backgroundColor: "#00ffea",position:"absolute", width:"14.2%", height:"85%", left:"28%"}}> </View>
+      <View style={{ backgroundColor: "#00ff00",position:"absolute", width:"14.2%", height:"85%", left:"42%"}}> </View>
+      <View style={{ backgroundColor: "#AF33FF",position:"absolute", width:"14.2%", height:"85%", left:"56%"}}> </View>
+      <View style={{ backgroundColor: "#FF0000",position:"absolute", width:"14.2%", height:"85%", left:"70%"}}> </View>
+      <View style={{ backgroundColor: "#0100FF",position:"absolute", width:"14.2%", height:"85%", left:"84%"}}> </View>
+
+      <View style={{ backgroundColor: "#0100FF",position:"absolute", width:"14.2%", height:"12%", bottom: "10.8%"}}> </View>
+      <View style={{ backgroundColor: "#000000",position:"absolute", width:"14.2%", height:"12%", bottom: "10.8%", left:"14%"}}> </View>
+      <View style={{ backgroundColor: "#AF33FF",position:"absolute", width:"14.2%", height:"12%", bottom: "10.8%", left:"28%"}}> </View>
+      <View style={{ backgroundColor: "#000000",position:"absolute", width:"14.2%", height:"12%", bottom: "10.8%", left:"42%"}}> </View>
+      <View style={{ backgroundColor: "#00ffea",position:"absolute", width:"14.2%", height:"12%", bottom: "10.8%", left:"56%"}}> </View>
+      <View style={{ backgroundColor: "#000000",position:"absolute", width:"14.2%", height:"12%", bottom: "10.8%", left:"70%"}}> </View>
+      <View style={{ backgroundColor: "#ffffff",position:"absolute", width:"14.2%", height:"12%", bottom: "10.8%", left:"84%"}}> </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
